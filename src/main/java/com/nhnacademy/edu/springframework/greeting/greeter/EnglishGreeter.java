de4958c4295ed5a454d0a532c08417130ede58ba
jeongwoo <sun62073812@gmail.com>
2024-01-04 16:52:43 +0900

    복습 : 객체의 생명주기 CallBacks 초기화 & 소멸

--- src/main/java/com/nhnacademy/edu/springframework/greeting/greeter/EnglishGreeter.java
+++ src/main/java/com/nhnacademy/edu/springframework/greeting/greeter/EnglishGreeter.java
@@ -1,9 +1,17 @@
 package com.nhnacademy.edu.springframework.greeting.greeter;
 
-public class EnglishGreeter implements Greeter{
+public class EnglishGreeter implements Greeter {
 
     public EnglishGreeter() {
         System.out.println("EnglishGreeter initiated!!");
+    }
+
+    public void init() {
+        System.out.println("EnglishGreeter init called!!");
+    }
+
+    public void cleanup() {
+        System.out.println("EnglishGreeter cleanup called!!");
     }
 
     @Override
--- src/main/java/com/nhnacademy/edu/springframework/greeting/greeter/KoreanGreeter.java
+++ src/main/java/com/nhnacademy/edu/springframework/greeting/greeter/KoreanGreeter.java
@@ -5,6 +5,14 @@ public class KoreanGreeter implements Greeter {
         System.out.println("KoreanGreeter initiated!!");
     }
 
+    public void init() {
+        System.out.println("KoreanGreeter init called!!");
+    }
+
+    public void cleanup() {
+        System.out.println("KoreanGreeter cleanup called!!");
+    }
+
     @Override
     public boolean sayHello() {
         System.out.println("한국말 안녕하세요!");
