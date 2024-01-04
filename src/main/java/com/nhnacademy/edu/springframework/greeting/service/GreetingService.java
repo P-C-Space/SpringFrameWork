82e5d4c69a60850ad59ccfc50af361d233b0ae7e
jeongwoo <sun62073812@gmail.com>
2024-01-05 00:53:53 +0900

    복습 : Injection Setter

--- src/main/java/com/nhnacademy/edu/springframework/greeting/service/GreetingService.java
+++ src/main/java/com/nhnacademy/edu/springframework/greeting/service/GreetingService.java
@@ -3,9 +3,17 @@ package com.nhnacademy.edu.springframework.greeting.service;
 import com.nhnacademy.edu.springframework.greeting.greeter.Greeter;
 
 public class GreetingService {
-    private final Greeter greeter;
+    private Greeter greeter;
+
+    public GreetingService() {
+    }
 
     public GreetingService(Greeter greeter) {
+        this.greeter = greeter;
+    }
+
+    public void setGreeter(Greeter greeter) {
+        System.out.println("setGreeter invoked!");
         this.greeter = greeter;
     }
 
