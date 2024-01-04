2c9791697219d2f1903b13ed8b8b2decc98a5e97
jeongwoo <sun62073812@gmail.com>
2024-01-05 00:44:32 +0900

    복습 : Injection Constructor

---
+++ src/main/java/com/nhnacademy/edu/springframework/greeting/main/ConstructorInjectionMain.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.edu.springframework.greeting.main;
+
+import com.nhnacademy.edu.springframework.greeting.service.GreetingService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConstructorInjectionMain {
+    public static void main(String[] args) {
+        try(ClassPathXmlApplicationContext context
+                    = new ClassPathXmlApplicationContext("greetBeans.xml")) {
+            GreetingService service = context.getBean("greetingService", GreetingService.class);
+            service.greet();
+        }
+    }
+}
